7ddb1ebca7016a90fa890225907dcd57d9d7f518
“Konstantinos <kostaks@outlook.com>
2023-01-31 17:31:48 +0100

    unexpected push

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { render } = require('ejs');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
@@ -7,16 +8,44 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+let allBeers
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts);
+//app.use(expressLayouts);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Add the route handlers here:
 
+
+// Add the route handlers here:
 app.get('/', (req, res) => {
-  res.render('index');
+  
+  res.render(__dirname + '/views/index.ejs')
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', async (req, res) => {
+  try{
+  allBeers = await punkAPI.getBeers();
+ // console.log("here is the beers", allBeers);
+  res.render(__dirname + '/views/beers.ejs');
+  }
+  catch(error){
+    console.log("Error getting the beers", error);
+  }
+});
+
+app.get('/random-beer', (req, res) => {
+  try{
+    //let randomBeer = allBeers[Math.floor(Math.random()*allBeers.length)]
+    //console.log("here is the beers", randomBeer);
+    res.render(__dirname + '/views/random-beer.ejs');
+  }
+  catch(error){
+    console.log("Error getting the beers", error);
+  }
+});
+
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
